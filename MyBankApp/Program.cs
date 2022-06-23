4384005486926f3aedcc2afc3a1ec11f7a3acd4d
Sarvesh Mishra <sarvesh.m@carwale.com>
2022-06-23 21:46:17 +0530

    add basic starting files

---
+++ MyBankApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyBankApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("My Bank App");
+        }
+    }
+}
